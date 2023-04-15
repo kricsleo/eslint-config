8f533268161ba3f34e20054f7bf5be1d57bad0d4
许圣峰 <kricsleo@163.com>
2023-04-15 14:50:12 +0800

    chore: add vue/max-attributes-per-line

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ module.exports = {
   extends: [
     '@antfu/eslint-config',
   ],
+  rules: {
+    // @see https://eslint.vuejs.org/rules/max-attributes-per-line.html
+    'vue/max-attributes-per-line': [
+      'error',
+      { singleline: { max: 3 } },
+    ],
+  }
 }
