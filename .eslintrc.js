a74defe5ef4674f9594d207d26a079991820da5e
kricsleo <kricsleo@163.com>
2025-07-27 22:59:33 +0800

    chore: bumpp setup

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['@antfu/eslint-config'],
-  rules: {
-    curly: ['error', 'all'],
-  },
-}
---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import eslint from '@antfu/eslint-config'
+
+export default eslint({
+  rules: {
+    curly: ['error', 'all'],
+  },
+})
