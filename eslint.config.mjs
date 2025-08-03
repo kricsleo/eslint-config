a549871fbc48e8580733590f97b839e0a19e3c3a
kricsleo <kricsleo@163.com>
2025-08-03 22:37:38 +0800

    chore(rules): disable`vue/html-self-closing`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,18 @@
 import antfu from '@antfu/eslint-config'
 
+/**
+ * Run `pnpm eslint --inspect-config` to inspect the configuration visually.
+ *
+ * @see https://eslint.org/blog/2024/04/eslint-config-inspector/
+ */
 export default antfu({
   vue: true,
+
+  rules: {
+    /**
+     * It's safe to disable this rule,
+     * as we are always using "Vue", not raw html.
+     */
+    'vue/html-self-closing': 'off',
+  },
 })
